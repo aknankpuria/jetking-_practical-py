6c675e9f0944406eae31c9bdd27a74c7c7409077
Aslam Khan shah <aksi34012@gmail.com>
2023-07-19 12:36:09 +0530

    first commit

---
+++ Q1.py
@@ -0,0 +1,18 @@
+#In Python Programming, how many types of errors are possible and define them.
+
+#1 syntax error
+    #syntax error occurs when the code is not in proper format.
+
+x=15
+
+# if x==15
+#     print("x is 15")
+
+# in upper code there is syntax error because we have not use : in if statement.
+
+if x==15:
+    print("x is 15")    
+
+    #now this is error free
+    
+
